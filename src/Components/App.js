fb2afc60d67e80d11e0b9db2c61db094886718f2
kamrul1 <kamrul01@yahoo.co.uk>
2020-05-16 04:18:00 +0100

    start of #4 lists

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import "../styles.css";
 
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises";
+import { muscles, exercises } from "../store";
 
-export default props => (
-  <Fragment>
-    <Header />
-    <Exercises />
-    <Footer />
-  </Fragment>
-);
+export default class extends Component {
+  state = {
+    exercises
+  };
+
+  getExcerisesByMuscles() {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise;
+
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles], exercise]
+          : [exercise];
+
+        return exercises;
+      }, {})
+    );
+  }
+
+  render() {
+    const excercies = this.getExcerisesByMuscles();
+
+    return (
+      <Fragment>
+        <Header />
+        <Exercises excercies={excercies} />
+        <Footer muscles={muscles} />
+      </Fragment>
+    );
+  }
+}
--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,23 +1,50 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
-import LeftPane from "./LeftPane";
-import RightPane from "./RightPane";
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
 
 const styles = {
   Paper: {
     padding: 20,
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
   }
 };
 
-export default props => (
+export default ({ excercies }) => (
   <Grid container>
     <Grid item sm>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {excercies.map(([group, excercises]) => (
+          <Fragment>
+            <Typography variant="h6" style={{ textTransform: "capitalize" }}>
+              {group}
+            </Typography>
+            <List component="ul">
+              {excercises.map(({ title }) => (
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              ))}
+            </List>
+          </Fragment>
+        ))}
+      </Paper>
     </Grid>
     <Grid item sm>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>
+        <Typography variant="h3">Welcome:</Typography>
+        <Typography variant="subtitle1" style={{ marginTop: 20 }}>
+          Please select an excercise from the list on the left
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
 );
